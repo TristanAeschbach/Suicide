952089bfab9997401ce1090f622d6f8055e63535
Tris10 <tristan.aeschbach@gmail.com>
2022-01-18 08:45:58 +0100

    Suicide

---
+++ suicideStarter.php
@@ -0,0 +1,21 @@
+<script>
+window.onload = function(){
+    for(let i=0; i<10; i++){
+        const posX = Math.random() * 1620;
+        const posY = Math.random() * 980;
+        window.open("Suicide.php","_blank", "width=200,height=200,left="+posX+",top="+posY);
+    }
+}
+</script>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Suicide</title>
+  </head>
+  <body>
+    You just started Suicide
+  </body>
+</html>
